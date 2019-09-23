3fa8d7dc51dda3e26c3e50959288261487502967
Aaron Clark <aaronpclark92@gmail.com>
2019-09-23 18:47:37 -0500

    added ANT design config files, added styling to ArticleCard.js

--- pintereach-app/src/components/ArticleCard.js
+++ pintereach-app/src/components/ArticleCard.js
@@ -1,45 +1,53 @@
 import React from 'react';
-import { Card, Icon, Tag, Button } from 'antd';
+import { Card, Icon, Button } from 'antd';
 import { Link } from 'react-router-dom';
 
 const ArticleCard = (props) => {
-  const { id, mustRead, imgUrl, title, category, summary } = props;
+  const { id, mustRead, imgUrl, title, summary } = props;
+
+  const { Meta } = Card;
 
   function preventDefault(e) {
     e.preventDefault();
     console.log('Clicked! But prevent default.');
   }
 
+  console.log(props)
+
   return (
     // card fragment
-    <div key={id}>
-      <Card
-        hoverable
-        style={{ width: 300 }}>
-        {/* 'must read' button, delete    div  should have absolute positioning at top left and right respectively*/}
-        {/* Must Read should be conditional on the boolean of article.mustRead */}
-        <div>
-          {/* toggle mustRead: true or false */}
-          <Button type={mustRead ? 'primary' : 'dashed'}>Must Read</Button>
-          {/* link delete function to Icon */}
-          <Icon type="delete" />
-        </div>
-        {/* image, title, tags, description div  insdie link to open article in new tab*/}
-        <Link to=''>
-          <div>
-            <img src={imgUrl} alt='article preview' style={{ width: 300 }} />
-            <h3>{title}</h3>
-            {/* <Icon type="tag" />  */}
-            {/* props.article.tags.map(entry => (
-              <Tag>
-            )) */}
-            <h5>{category}</h5>
-            <p>{summary}</p>
-          </div>
-        </Link>
-      </Card>
 
-    </div>
+    <Card
+      key={id}
+      hoverable
+      cover={
+        <img src={imgUrl} alt='article preview' style={{ maxHeight: 180, width: 'auto', margin: 'auto' }} />
+      }
+      style={{ width: 320, marginBottom: '1rem' }}
+      actions={[
+        <Button type={(mustRead ? 'primary' : 'dashed')} >Must Read</Button>,
+        <Icon type="delete" />
+
+
+      ]}>
+      <Meta
+        title={title}
+        description={summary}
+
+      />
+      {/* 'must read' button, delete    div  should have absolute positioning at top left and right respectively*/}
+      {/* Must Read should be conditional on the boolean of article.mustRead */}
+
+      {/* toggle mustRead: true or false */}
+      {/* <Button type={(mustRead ? 'primary' : 'dashed')}>Must Read</Button> */}
+      {/* link delete function to Icon */}
+
+      {/* <Icon type="delete" /> */}
+      {/* image, title, tags, description div  insdie link to open article in new tab*/}
+
+    </Card >
+
+
   )
 
 }
