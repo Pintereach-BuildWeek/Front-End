6d146db36160d41c0e6a1cc78c8ad3b659472cda
Chrismis79 <chrismis79@gmail.com>
2019-09-24 09:06:04 -0500

    ArticleList component built

---
+++ pintereach-app/src/components/ArticleList.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import SearchForm from "./SearchForm";
+import ArticleCard from './ArticleCard';
+
+function ArticleList() {
+    const [articleList, setArticleList] = useState(data);
+    const {id, mustRead, imgUrl, title, category, summary} = data;
+
+
+    return (
+        <>
+        <SearchForm/>
+        <div>
+            {
+                data.map(entry =>
+                    <ArticleCard id={entry.id} 
+                                 mustRead={entry.mustRead} 
+                                 imgUrl={entry.imgUrl} 
+                                 title={entry.title} 
+                                 category={entry.category} 
+                                 summary={entry.summary}/>
+                        )
+
+            }
+        </div>
+        </>
+    )
+}
+export default ArticleList;
