cbb75a4f66e9307b3a576e61d85a251ee16a0650
Aaron Clark <aaronpclark92@gmail.com>
2019-09-24 14:15:17 -0500

    styled Signup.js

--- pintereach-app/src/components/Signup.js
+++ pintereach-app/src/components/Signup.js
@@ -4,48 +4,66 @@ import React from "react";
 
 import { connect } from "react-redux";
 
+import { Button, Layout, Form, Icon, Input } from 'antd';
+
+
 import { signup } from "../actions";
 
 // SIGNUP COMPONENT
 
-class Signup extends React.Component {
-    state = {
-      signupCreds: {
-        email: "",
-        username: "",
-        password: ""
-      }
-    };
-  
-    changeHandler = e => {
-      this.setState({
-        signupCreds: {
-          ...this.state.signupCreds,
-          [e.target.name]: e.target.value
-        }
-      });
-    };
-  
-    signup = e => {
-      // e.preventDefault(); <- IF NEEDED
-      this.props
-        .signup(this.state.signupCreds)
-        .then(() => this.props.history.push(""));  //  <- ADD PATH
-    };
-  
-    render() {
-      return (
-        <>
-          <div className="sign-up">
 
-            <section className="sign-up-form">
+const { Content } = Layout;
+
+class Signup extends React.Component {
+  state = {
+    signupCreds: {
+      email: "",
+      username: "",
+      password: ""
+    }
+  };
+
+  changeHandler = e => {
+    this.setState({
+      signupCreds: {
+        ...this.state.signupCreds,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  signup = e => {
+    // e.preventDefault(); <- IF NEEDED
+    this.props
+      .signup(this.state.signupCreds)
+      .then(() => this.props.history.push(""));  //  <- ADD PATH
+  };
+
+  render() {
+    return (
+      <>
+        <div className="sign-up">
+          <Layout>
+            <Content
+              style={{
+                width: 480,
+                marginRight: 'auto',
+                marginLeft: 'auto',
+                marginTop: '2rem',
+                marginBottom: '2rem'
+              }}
+              className="sign-up-form">
 
               <h1>
                 Sign Up for Pintereach
               </h1>
 
-              <form onSubmit={this.signup}>
-                <input
+              <Form
+                layout='horizontal'
+                onSubmit={this.signup}>
+                <Input
+                  prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  style={{ marginBottom: '1rem' }}
                   type="email"
                   name="email"
                   placeholder="Email"
@@ -53,7 +71,10 @@ class Signup extends React.Component {
                   onChange={this.changeHandler}
                   required
                 />
-                <input
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  style={{ marginBottom: '1rem' }}
+
                   type="text"
                   name="username"
                   placeholder="Username"
@@ -61,7 +82,10 @@ class Signup extends React.Component {
                   onChange={this.changeHandler}
                   required
                 />
-                <input
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  style={{ marginBottom: '1rem' }}
+
                   type="password"
                   name="password"
                   placeholder="Password"
@@ -69,28 +93,30 @@ class Signup extends React.Component {
                   onChange={this.changeHandler}
                   required
                 />
-                <button>Sign Up</button>
-              </form>
-            </section>
+                <Button
 
-          </div>
+                >Sign Up</Button>
+              </Form>
+            </Content>
+          </Layout>
+        </div>
 
-          <footer>
-            <p> Add Footer Content !!! </p>
-          </footer>
+        <footer>
+          <p> Add Footer Content !!! </p>
+        </footer>
 
-        </>
-      );
-    }
+      </>
+    );
   }
-  
-  const mapStateToProps = (state) => ({
-    //email: state.email,
-    //username: state.username,
-    //password: state.password
-  });
-  
-  export default connect(
-    mapStateToProps,
-    { signup }
-  )(Signup);
+}
+
+const mapStateToProps = (state) => ({
+  //email: state.email,
+  //username: state.username,
+  //password: state.password
+});
+
+export default connect(
+  mapStateToProps,
+  { signup }
+)(Signup);
