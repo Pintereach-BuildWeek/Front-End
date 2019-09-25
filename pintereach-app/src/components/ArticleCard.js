0e4ebd6f8f2cbc3efc8f4f545127730a00297914
Aaron Clark <aaronpclark92@gmail.com>
2019-09-24 22:47:30 -0500

    added functioning Menu Drawer and Menu Modal, functions assigned to button and are working with dummy data, filtering by mustRead status works

--- pintereach-app/src/components/ArticleCard.js
+++ pintereach-app/src/components/ArticleCard.js
@@ -1,53 +1,67 @@
 import React from 'react';
-import { Card, Icon, Button } from 'antd';
+import { Card, Icon, Button, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 
 const ArticleCard = (props) => {
-  const { id, mustRead, imgUrl, title, summary } = props;
+  const { id, mustRead, imgUrl, title, summary, category, setMustRead, deleteArticle } = props;
 
   const { Meta } = Card;
+
 
   function preventDefault(e) {
     e.preventDefault();
     console.log('Clicked! But prevent default.');
   }
 
-  console.log(props)
-
   return (
     // card fragment
-
-    <Card
-      key={id}
-      hoverable
-      cover={
-        <img src={imgUrl} alt='article preview' style={{ maxHeight: 180, width: 'auto', margin: 'auto' }} />
-      }
-      style={{ width: 320, marginBottom: '1rem' }}
-      actions={[
-        <Button type={(mustRead ? 'primary' : 'dashed')} >Must Read</Button>,
-        <Icon type="delete" />
-
-
-      ]}>
-      <Meta
-        title={title}
-        description={summary}
-
-      />
-      {/* 'must read' button, delete    div  should have absolute positioning at top left and right respectively*/}
-      {/* Must Read should be conditional on the boolean of article.mustRead */}
-
-      {/* toggle mustRead: true or false */}
-      {/* <Button type={(mustRead ? 'primary' : 'dashed')}>Must Read</Button> */}
-      {/* link delete function to Icon */}
-
-      {/* <Icon type="delete" /> */}
-      {/* image, title, tags, description div  insdie link to open article in new tab*/}
-
-    </Card >
+    <>
+      <Col xs={18} sm={14} md={10} lg={8} xl={7} >
+        <Card
+          hoverable
+          cover={
+            <img src={imgUrl} alt='article preview' style={{ maxHeight: 180, width: 'auto', margin: 'auto' }} />
+          }
+          style={{ width: 320, marginBottom: '1rem' }}
+          actions={[
+            <Button type={(mustRead ? 'primary' : 'dashed')} onClick={() => setMustRead(id)} >Must Read</Button>,
+            <Icon type="delete" onClick={() => deleteArticle(id)} />
 
 
+          ]}>
+          <Meta
+            title={title}
+            description={summary}
+
+          />
+          <div>
+            <hr style={{
+              margin: '1rem'
+            }}></hr>
+            <span
+              style={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                alignItems: 'baseline'
+              }}>
+              <Icon type="tag" />
+              <p>{category}</p>
+            </span>
+          </div>
+          {/* 'must read' button, delete    div  should have absolute positioning at top left and right respectively*/}
+          {/* Must Read should be conditional on the boolean of article.mustRead */}
+
+          {/* toggle mustRead: true or false */}
+          {/* <Button type={(mustRead ? 'primary' : 'dashed')}>Must Read</Button> */}
+          {/* link delete function to Icon */}
+
+          {/* <Icon type="delete" /> */}
+          {/* image, title, tags, description div  insdie link to open article in new tab*/}
+
+        </Card >
+      </Col>
+
+    </>
   )
 
 }
--- pintereach-app/src/components/Home.js
+++ pintereach-app/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button } from 'antd';
 
 import ArticleList from './ArticleList';
 import ArticleModal from './ArticleModal';
@@ -9,27 +10,86 @@ import ArticleCard from './ArticleCard';
 import { articlesArray } from '../data';
 
 const Home = () => {
-  // State for Menu, ArticleModal
   const [articles, setArticles] = useState(articlesArray);
-  // State for articleList
+  // State for Menu, ArticleModal
+
+  const [menuDisplay, setMenuDisplay] = useState({ visible: false, placement: 'left' });
+
+  //ArticleModal state
+  const [modalDisplay, setModalDisplay] = useState({ visible: false });
+
+  // Toggle State
+  const [toggleState, setToggleState] = useState(false)
+
+  // Modal functions
+
+  const showModal = () => {
+    // console.log(modalDisplay);
+    setModalDisplay({ visible: true });
+  }
+
+  const hideModal = () => {
+    // console.log(modalDisplay);
+    setModalDisplay({ visible: false });
+  }
 
   const addArticle = article => {
+    console.log('submit and add article pressed')
     article.id = articles.length + 1;
-    setArticles([...articles, article])
+    setArticles([...articles, article]);
+    setModalDisplay({ visible: false });
   }
+
+  // Card Functions
 
   const deleteArticle = id => {
     console.log(`delete clicked`)
     setArticles(articles.filter(article => article.id !== id))
   }
 
+  const setMustRead = id => {
+    console.log(`mustRead clicked`);
+    console.log(id)
+    console.log(articles[id].mustRead);
+    articles[id].mustRead = !articles[id].mustRead
+  }
+
+  // Menu functions
+  const showMenu = () => {
+    console.log(menuDisplay);
+    setMenuDisplay({ visible: true });
+  }
+
+  const hideMenu = () => {
+    console.log(menuDisplay);
+    setMenuDisplay({ visible: false });
+  }
+
+  const filterMustRead = () => {
+    console.log(`delete clicked`)
+    setToggleState(!toggleState)
+    !toggleState ?
+      setArticles(articles.filter(article => article.mustRead === true)) : setArticles(articlesArray)
+    console.log(toggleState)
+  }
+
+
+
   return (
     <>
-      {/* <ArticleModal />
+      {/* 
       <Menu /> */}
+      <Button onClick={() => showModal()}>Add Article</Button>
+      <Button onClick={() => showMenu()}>Menu</Button>
+
+      <Menu showMenu={showMenu} hideMenu={hideMenu} menuDisplay={menuDisplay} articles={articles} showModal={showModal} filterMustRead={filterMustRead} />
+
+      <ArticleModal addArticle={addArticle} modalDisplay={modalDisplay} showModal={showModal} hideModal={hideModal} />
+
       {
         articles.map(entry =>
           <ArticleCard
+            setMustRead={setMustRead}
             deleteArticle={deleteArticle}
             key={entry.id}
             id={entry.id}
