a61791d6ce5a06ca462dbd6d746136c7e358465c
shaunorpen <shaunorpen@users.noreply.github.com>
2019-09-25 13:48:24 +0100

    Tidy up ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,34 +1,31 @@
-import React, {useState} from "react";
+import React from "react";
 // import SearchForm from "./SearchForm";
 import ArticleCard from './ArticleCard';
 
-
 function ArticleList({ data, setMustRead, deleteArticle }) {
-    // const [articleList, setArticleList] = useState(data);
-    // const {id, mustRead, imgUrl, title, category, summary} = data;
 
     return (
         <>
-        {/* <SearchForm/> */}
-        <div>
-            {
-                
-                data.map(entry => 
-                    <ArticleCard    setMustRead={setMustRead}
-                                    deleteArticle={deleteArticle}
-                                    key={entry.articleid}
-                                    id={entry.articleid}
-                                    mustRead={entry.articleid % 2 === 0 ? false : true}
-                                    imgUrl='https://source.unsplash.com/random'
-                                    title='Title goes here.'
-                                    category={entry.category}
-                                    summary='Summary goes here.'
-                                    link={entry.link}
-                                    />
-                        )
+            {/* <SearchForm/> */}
+            <div>
+                {
+                    
+                    data.map(entry => 
+                        <ArticleCard    setMustRead={setMustRead}
+                                        deleteArticle={deleteArticle}
+                                        key={entry.articleid}
+                                        id={entry.articleid}
+                                        mustRead={entry.id % 2 === 0 ? false : true}
+                                        imgUrl='https://source.unsplash.com/random'
+                                        title='Title goes here.'
+                                        category={entry.category}
+                                        summary='Summary goes here.'
+                                        link={entry.link}
+                                        />
+                            )
 
-            }
-        </div>
+                }
+            </div>
         </>
     )
 }
