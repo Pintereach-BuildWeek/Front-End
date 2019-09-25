5a8cf87f5ef9050adf9e12b31e8cd0392fb823ce
Nicholas Truson <nicholas.truson@gmail.com>
2019-09-24 20:55:40 -0700

    axios add delete, navlinks

--- pintereach-app/src/reducers/index.js
+++ pintereach-app/src/reducers/index.js
@@ -15,7 +15,26 @@ const initialState = {
 // REDUCER
 
 const reducer = (state = initialState, action) => {
-
-};
+    switch (action.type) {
+        case LOGIN_START:
+          return {
+            ...state,
+            loggingIn: true
+          };
+        case SIGNUP_START:
+          return {
+            ...state,
+            loggingIn: true,
+            signingUp: true
+          };
+        case LOGIN_SUCCESS:
+          return {
+            ...state,
+            loggingIn: false,
+            token: action.payload.token,
+            message: action.payload.message,
+            id: action.payload.id
+          };
+}};
 
 export default reducer;
